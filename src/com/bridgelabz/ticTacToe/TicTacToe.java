e6bb143879c35ef704df3884c3a8fb896290bb1c
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-24 20:05:50 +0530

    refactor : program for select user to make a move to desired location

--- src/com/bridgelabz/ticTacToe/TicTacToe.java
+++ src/com/bridgelabz/ticTacToe/TicTacToe.java
@@ -8,7 +8,7 @@ public class TicTacToe {
     static char player, computer;
     static Scanner scanner = new Scanner(System.in);
 
-    static void startGame() {
+    static void selectIndex() {
         for (int i = 1; i <= 9; i++) {
             board[i] = ' ';
     
@@ -18,18 +18,17 @@ public class TicTacToe {
     // creating method to select x & O
 
     public static void check() {
-        System.out.println("Please Select Your Letter : 'X' or 'O' ");
+        System.out.println("Please Select Your Captial Letter : 'X' or 'O' ");
         char choice = scanner.next().charAt(0);
-        if (choice == 'X') {
-            player = 'X';
+        if (player == 'X') {
             computer = 'O';
-        } else if (choice == 'Y') {
-            player = 'O';
+        } else
             computer = 'X';
-        }
+            System.out.println("You have selected : " + player);
+            System.out.println("Computer's choice is : " + computer);
     }
 
-    public void showBoard(){
+    public static void showBoard() {
 		System.out.println("Welcome to Tic Tac Toe ");
 		System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
 		System.out.println("---------");
@@ -37,10 +36,22 @@ public class TicTacToe {
 		System.out.println("---------");
 		System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
 	}
+    
+    public static void userMove() {
+        System.out.println("Enter index from 1-9 to make the Move");
+        int playLocation = scanner.nextInt();
+        if (playLocation < 10 && playLocation > 0) {
+            board[playLocation] = player;
+            showBoard();
+        } else {
+            System.out.println("Invalid Choice");
+        }
+    }
     public static void main(String[] args){
-        TicTacToe newObj = new TicTacToe();
-        newObj.startGame();
-        newObj.check();
-        newObj.showBoard();
+        System.out.println("Welcome To Tic Tac Toe Game");
+        selectIndex();
+        check();
+        showBoard();
+        userMove();
     }
 }
