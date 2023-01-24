7a4f3a2bb1309d88731050d89ced99755d6fbf3e
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-24 19:53:47 +0530

    refactor : program for display current board

--- src/com/bridgelabz/ticTacToe/TicTacToe.java
+++ src/com/bridgelabz/ticTacToe/TicTacToe.java
@@ -3,6 +3,7 @@ package com.bridgelabz.ticTacToe;
 import java.util.Scanner;
 
 public class TicTacToe {
+    
     private static char[] board = new char[10];
     static char player, computer;
     static Scanner scanner = new Scanner(System.in);
@@ -10,7 +11,7 @@ public class TicTacToe {
     static void startGame() {
         for (int i = 1; i <= 9; i++) {
             board[i] = ' ';
-            System.out.println(board);
+    
         }
     }
     // Taking input from user
@@ -28,14 +29,18 @@ public class TicTacToe {
         }
     }
 
-    public static void main(String[] args) {
+    public void showBoard(){
 		System.out.println("Welcome to Tic Tac Toe ");
 		System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
 		System.out.println("---------");
 		System.out.println(board[4]+" | "+board[5]+" | "+board[6]);
 		System.out.println("---------");
 		System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
-		startGame();
-        check();
 	}
+    public static void main(String[] args){
+        TicTacToe newObj = new TicTacToe();
+        newObj.startGame();
+        newObj.check();
+        newObj.showBoard();
+    }
 }
